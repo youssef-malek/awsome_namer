58ba8f23763acb76db6a78b8d5c77f3154811fbc
youssefAMalek <youssef.ait.malek1@gmail.com>
2025-06-07 22:09:45 +0100

    Center column

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
 
-  getNext(){
+  getNext() {
     current = WordPair.random();
     notifyListeners();
   }
@@ -38,19 +38,48 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
-
+    var pair = appState.current;
     return Scaffold(
-      body: Column(
-        children: [
-          
-          Text('A random idea:'), Text(appState.current.asLowerCase),
-          Text(appState.current.asLowerCase),
-          ElevatedButton(onPressed: (){
-              appState.getNext();
-          }, child: Text("Next"))
-
-           
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(appState.current.asLowerCase),
+            BigCard(pair: pair),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                appState.getNext();
+              },
+              child: Text("Next"),
+            ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({super.key, required this.pair});
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
       ),
     );
   }
