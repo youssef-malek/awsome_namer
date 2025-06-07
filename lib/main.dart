c338e8f7f9c696ae9b17d60568ca02e045000358
youssefAMalek <youssef.ait.malek1@gmail.com>
2025-06-07 16:09:47 +0100

    Button to change random word added

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,11 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  getNext(){
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
@@ -36,7 +41,16 @@ class MyHomePage extends StatelessWidget {
 
     return Scaffold(
       body: Column(
-        children: [Text('A random idea:'), Text(appState.current.asLowerCase)],
+        children: [
+          
+          Text('A random idea:'), Text(appState.current.asLowerCase),
+          Text(appState.current.asLowerCase),
+          ElevatedButton(onPressed: (){
+              appState.getNext();
+          }, child: Text("Next"))
+
+           
+          ],
       ),
     );
   }
